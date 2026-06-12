c1f7259fd4368c6e9af6d73b3442371c579024e0
nahuheredia01 <nahuheredia01@gmail.com>
2026-06-12 18:39:53 -0300

    PostForm creado

--- ExamenProgWeb/src/components/PostForm.jsx
+++ ExamenProgWeb/src/components/PostForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function PostForm() {
+function PostForm({ onSubmit }) {
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit({
+      title,
+      body
+    });
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={handleSubmit}>
+
+      <input
+        type="text"
+        placeholder="Titulo"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <br />
+
+      <textarea
+        placeholder="Contenido"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+
+      <br />
+
+      <button type="submit">
+        Guardar
+      </button>
+
+    </form>
+  );
 }
+
+export default PostForm;
